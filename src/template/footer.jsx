fb8d04bf496cd9fd1bfeb737a6a10832cc7c32ed
alireza_moghilan <alirezamoghilan0085@gmail.com>
2025-05-28 15:26:09 +0330

    add gap to navigation items in Footer for improved spacing

--- src/template/footer.jsx
+++ src/template/footer.jsx
@@ -6,7 +6,7 @@ export const Footer = (ev) => {
         <>
             <footer className="border-top bg-light">
                 <div className='p-4 row flex-wrap justify-content-between align-items-center'>
-                    <ul className="nav col-md-4 col-4 justify-content-start">
+                    <ul className="nav col-md-4 col-4  gap-2 justify-content-start">
                         <li className="nav-item"><a href='https://instagram.com/moghilan_web?utm_source=qr&amp;amp;amp;amp;amp;amp;igshid=MzNlNGNkZWQ4Mg%3D%3D' target='_blank' className="nav-link pe-3 py-0 text-body-secondary"><i className="bi bi-instagram h3 text-main d-flex align-items-center mb-0"></i></a></li>
                         <li className="nav-item"><a href="https://github.com/alireza-moghilan" target='_blank' className="nav-link pe-3 py-0 text-body-secondary"><i className="bi bi-github h3 text-main d-flex align-items-center mb-0"></i></a></li>
                         <li className="nav-item"><a href="https://www.linkedin.com/in/alireza-moghilan" target='_blank' className="nav-link pe-3 py-0 text-body-secondary"><i className="bi bi-linkedin h3 text-main d-flex align-items-center mb-0"></i></a></li>
